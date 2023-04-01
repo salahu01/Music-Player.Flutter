e6531efd1effbc0676d92ae74d4bc4726381b697
salahu01 <cvsalahu12@gmail.com>
2023-04-01 17:57:54 +0530

    added notification image

--- lib/core/helpers/asset_helper.dart
+++ lib/core/helpers/asset_helper.dart
@@ -1,4 +1,5 @@
 class AssetHelper {
   static const lightIcon = 'assets/images/icon_white.png';
   static const darkIcon = 'assets/images/icon_black.png';
+  static const noticationImage = 'https://ik.imagekit.io/vun0le607a/Github/Musician/app_icons/splash_icon_white_Ldfg7FfO-.png?ik-sdk-version=javascript-1.4.3&updatedAt=1674728487749';
 }
